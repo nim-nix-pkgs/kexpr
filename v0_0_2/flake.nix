8d0f693d2955d52e5d6c50c43df767f5b015d460
hugosenari <hugosenari@gmail.com>
2022-03-12 01:32:53 -0300

    chore: re index kexpr

---
+++ v0_0_2/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''wrapper for kexpr math expression evaluation library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-kexpr-v0_0_2.flake = false;
+  inputs.src-kexpr-v0_0_2.owner = "brentp";
+  inputs.src-kexpr-v0_0_2.ref   = "refs/tags/v0.0.2";
+  inputs.src-kexpr-v0_0_2.repo  = "kexpr-nim";
+  inputs.src-kexpr-v0_0_2.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-kexpr-v0_0_2"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-kexpr-v0_0_2";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
